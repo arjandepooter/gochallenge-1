6f3d21c0a81a63524f882e7b6d7766038293d1bc
Arjan de Pooter <arjan@cmsworks.nl>
2015-03-11 09:47:08 +0100

    Moved types to own file

--- decoder.go
+++ decoder.go
@@ -5,21 +5,6 @@ import (
 	"os"
 )
 
-// Pattern is the high level representation of the
-// drum pattern contained in a .splice file.
-type Pattern struct {
-	Version string
-	Tempo   float32
-	Tracks  []*Track
-}
-
-// Track is a representation of a single track in a pattern
-type Track struct {
-	ID    int
-	Name  string
-	Steps [16]bool
-}
-
 // DecodeFile decodes the drum machine file found at the provided path
 // and returns a pointer to a parsed pattern which is the entry point to the
 // rest of the data.
