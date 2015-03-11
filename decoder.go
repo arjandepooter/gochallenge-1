d377ff4a4f8932faae12ef7a52ed42e2a0a5c6cd
Arjan de Pooter <arjan@cmsworks.nl>
2015-03-11 09:41:05 +0100

    Restructured code

--- decoder.go
+++ decoder.go
@@ -1,12 +1,24 @@
 package drum
 
 import (
-	"bytes"
-	"encoding/binary"
 	"fmt"
-	"io"
 	"os"
 )
+
+// Pattern is the high level representation of the
+// drum pattern contained in a .splice file.
+type Pattern struct {
+	Version string
+	Tempo   float32
+	Tracks  []*Track
+}
+
+// Track is a representation of a single track in a pattern
+type Track struct {
+	ID    int
+	Name  string
+	Steps [16]bool
+}
 
 // DecodeFile decodes the drum machine file found at the provided path
 // and returns a pointer to a parsed pattern which is the entry point to the
@@ -32,6 +44,9 @@ func DecodeFile(path string) (*Pattern, error) {
 	defer f.Close()
 
 	header, err := readHeader(f)
+	if err != nil {
+		return nil, err
+	}
 	if header != "SPLICE" {
 		return nil, fmt.Errorf("Invalid file header, expected SPLICE, got %s", header)
 	}
@@ -68,102 +83,4 @@ func DecodeFile(path string) (*Pattern, error) {
 	p.Tracks = tracks
 
 	return p, nil
-}
-
-func readHeader(file io.Reader) (string, error) {
-	buf := make([]byte, 6)
-	_, err := file.Read(buf)
-
-	if err != nil {
-		return "", err
-	}
-
-	return string(buf), nil
-}
-
-func readContentSize(file io.Reader) (int64, error) {
-	var size int64
-	err := binary.Read(file, binary.BigEndian, &size)
-
-	if err != nil {
-		return 0, err
-	}
-
-	return size, nil
-}
-
-func readVersion(file io.Reader) (string, error) {
-	buf := make([]byte, 32)
-	_, err := file.Read(buf)
-
-	if err != nil {
-		return "", err
-	}
-
-	return string(bytes.Trim(buf, "\x00")), nil
-}
-
-func readTempo(file io.Reader) (float32, error) {
-	var tempo float32
-	err := binary.Read(file, binary.LittleEndian, &tempo)
-
-	if err != nil {
-		return 0, err
-	}
-
-	return tempo, nil
-}
-
-func readTrack(file io.Reader, size *int64) (*Track, error) {
-	track := new(Track)
-
-	var id int32
-	err := binary.Read(file, binary.LittleEndian, &id)
-	if err != nil {
-		return nil, err
-	}
-	track.ID = int(id)
-	*size -= 4
-
-	var nameLength int8
-	err = binary.Read(file, binary.LittleEndian, &nameLength)
-	if err != nil {
-		return nil, err
-	}
-	*size--
-
-	buf := make([]byte, nameLength)
-	file.Read(buf)
-	track.Name = string(buf)
-	*size -= int64(nameLength)
-
-	var steps [16]bool
-	for i := 0; i < 16; i++ {
-		var buf int8
-		err = binary.Read(file, binary.LittleEndian, &buf)
-		if err != nil {
-			return nil, err
-		}
-
-		steps[i] = (buf > 0)
-	}
-	*size -= 16
-	track.Steps = steps
-
-	return track, nil
-}
-
-// Pattern is the high level representation of the
-// drum pattern contained in a .splice file.
-type Pattern struct {
-	Version string
-	Tempo   float32
-	Tracks  []*Track
-}
-
-// Track is a representation of a single track in a pattern
-type Track struct {
-	ID    int
-	Name  string
-	Steps [16]bool
 }
